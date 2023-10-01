ed6323e71f6631bd941ca392183312b32d0c01ca
changwoo <ep6tri@hotmail.com>
2023-10-02 00:41:59 +0900

    1.0.1 commit

--- includes/templates/check.php
+++ includes/templates/check.php
@@ -9,11 +9,11 @@
 ?>
 
 <div class="d1m1">
-    <h2>1D1M Check</h2>
+    <h1>1D1M Check</h1>
     <p class="total-count">
-        <?php
-	    printf( _nx( 'Total %d song collected.', 'Total %d songs collected.', 'Total number of songs.', 'm1d1' ), $total_count );
-	    ?>
+		<?php
+		printf( _nx( 'Total %d song collected.', 'Total %d songs collected.', 'Total number of songs.', 'm1d1' ), $total_count );
+		?>
     </p>
     <form action="" method="get">
         <fieldset>
@@ -22,8 +22,8 @@
             <input id="keyword" name="keyword" type="search" autocomplete="off"
                    value="<?php echo esc_attr( $keyword ); ?>">
         </fieldset>
-        <p>
-            <button type="submit">Search</button>
+        <p class="button-wrap">
+            <button type="submit">Submit</button>
         </p>
     </form>
 
@@ -37,22 +37,27 @@
             <table class="">
                 <thead>
                 <tr>
-                    <th>Seq.</th>
-                    <th>Artist</th>
-                    <th>Title</th>
-                    <th>Rating</th>
-                    <th>Link</th>
+                    <th class="col-seq">Seq.</th>
+                    <th class="col-artist">Artist</th>
+                    <th class="col-title">Title</th>
+                    <th class="col-rating">Rating</th>
                 </tr>
                 </thead>
                 <tbody>
 				<?php foreach ( $rows as $row ): ?>
-                <tr>
-                    <td><?php echo esc_html( $row->sequence ); ?></td>
-                    <td><?php echo esc_html( $row->artist ); ?></td>
-                    <td><?php echo esc_html( $row->title ); ?></td>
-                    <td><?php echo esc_html( $row->rating ); ?></td>
-                    <td><?php echo esc_html( $row->yt_id ); ?></td>
-                </tr>
+                    <tr>
+                        <td class="col-seq"><?php echo esc_html( $row->sequence ); ?></td>
+                        <td class="col-artist"><?php echo esc_html( $row->artist ); ?></td>
+                        <td class="col-title">
+							<?php if ( $row->yt_id ) : ?>
+                                <a href="<?php echo esc_url( "https://music.youtube.com/watch?v=$row->yt_id" ); ?>"
+                                   target="_blank"><?php echo esc_html( $row->title ); ?></a>
+							<?php else: ?>
+								<?php echo esc_html( $row->title ); ?>
+							<?php endif; ?>
+                        </td>
+                        <td class="col-rating"><?php echo esc_html( $row->rating ); ?></td>
+                    </tr>
 				<?php endforeach; ?>
                 </tbody>
             </table>
--- m1d1.php
+++ m1d1.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:       1일1메탈 데이터베이스
- * Version:           1.0.0
+ * Version:           1.0.1
  * Description:       1일1메탈 수집곡을 보관하고 포스팅 아카이빙하는 워드프레스 플러그인.
  * Plugin URI:        https://github.com/chwnam/m1d1
  * Requires at least:
@@ -12,6 +12,9 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       m1d1
  */
+
+const M1D1_MAIN = __FILE__;
+const M1D1_VERSION = '1.0.1';
 
 if ( ! function_exists( 'm1d1_check' ) ) {
 	function m1d1_check(): string {
@@ -35,6 +38,9 @@ if ( ! function_exists( 'm1d1_check' ) ) {
 		ob_start();
 
 		include __DIR__ . '/includes/templates/check.php';
+
+		wp_enqueue_script( 'm1d1', plugins_url( 'includes/assets/script.js', M1D1_MAIN ), array(), M1D1_VERSION );
+		wp_enqueue_style( 'm1d1', plugins_url( 'includes/assets/style.css', M1D1_MAIN ), array(), M1D1_VERSION );
 
 		return ob_get_clean();
 	}
